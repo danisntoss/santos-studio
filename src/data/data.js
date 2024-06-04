0f57e3baf672906d31d3a2be68ff61aaed734457
danisntoss <danisant1504.dsl@gmail.com>
2024-06-04 13:09:05 +0200

    rutas dinamicas y actualizacion de los enlaces en una nueva página

--- src/data/data.js
+++ src/data/data.js
@@ -3,6 +3,24 @@ export const data = [
         id: "1",
         name: "Metro",
         image: "/imgs/metro.jpg",
-        description: "metro is a fast and efficient way to travel around the city"
+        description: "Creado para una parada de metro, para devolver la esencia de los antiguos carteles de metro de Madrid"
+    },
+    {
+        id: "2",
+        name: "Estanco",
+        image: "/imgs/estanco.jpeg",
+        description: "Creado para un particular, con el fin de arreglar el anterior letrero que tenia"
+    },
+    {
+        id: "3",
+        name: "La Plazuela",
+        image: "/imgs/plazuela.jpeg",
+        description: "Creado para una papelera, con el fin de personalizar su furgoneta de reparto"
+    },
+    {
+        id: "4",
+        name: "Pegatinas!",
+        image: "/imgs/pegatinas.jpeg",
+        description: "Algunas pegatinas que he hecho para personalizar objetos"
     },
 ]
