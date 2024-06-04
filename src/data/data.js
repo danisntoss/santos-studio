cec597357307ee903884aaacd97ec837a99d9bea
danisntoss <danisant1504.dsl@gmail.com>
2024-06-04 12:02:14 +0200

    added dynamic routes

---
+++ src/data/data.js
@@ -0,0 +1,8 @@
+export const data = [
+    {
+        id: "1",
+        name: "Metro",
+        image: "/imgs/metro.jpg",
+        description: "metro is a fast and efficient way to travel around the city"
+    },
+]
