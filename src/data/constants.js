24637891d0c175de50261525ab72189a9d11af24
CuB1z <cub1zdev@gmail.com>
2024-10-26 18:22:33 +0100

    Add constants and improve SEO

---
+++ src/data/constants.js
@@ -0,0 +1,23 @@
+export const CONSTANTS = {
+    socials: {
+        github: {
+            url: 'https://github.com/danisntoss',
+            icon: '/icons/github.svg'
+        },
+        linkedin: {
+            url: 'https://www.linkedin.com/in/danisantos15/',
+            icon: '/icons/linkedin.svg'
+        },
+        instagram: {
+            url: 'https://www.instagram.com/santos.studi0/',
+            icon: '/icons/instagram.svg'
+        }
+    },
+    webdata: {
+        title: 'Santos Studio',
+        author: 'Daniel Santos',
+        description: 'Estudio de diseño y corte de vinilo. Trato personalizado y productos de calidad | Daniel Santos',
+        keywords: 'Santos Studio, Daniel Santos, Diseño, Vinilo, Corte, Personalizado, Calidad',
+        url: 'https://santos-studio.es',
+    }
+}
