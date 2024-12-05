51381bbaddab2877dc02234085d8f3386cdc44e2
CuB1z <cub1zdev@gmail.com>
2024-12-05 15:05:40 +0100

    style: Add tags

--- src/data/data.js
+++ src/data/data.js
@@ -3,50 +3,57 @@ export const data = [[
         id: "1",
         name: "Estanco",
         image: "/imgs/estanco.jpeg",
-        description: "Diseñado y cortado para arreglar el antiguo cartel que tenían puesto en la fachada."
+        description: "Diseñado y cortado para arreglar el antiguo cartel que tenían puesto en la fachada.",
+        tags: ["Cartel", "Premium"]
     },
     {
         id: "2",
         name: "Metro",
         image: "/imgs/metro.jpg",
-        description: "Creado para una furgoneta de la empresa."
+        description: "Creado para una furgoneta de la empresa.",
+        tags: ["Logo", "Estándar"]
     },
     {
         id: "3",
         name: "La Plazuela",
         image: "/imgs/plazuela.jpeg",
-        description: "Creado para una papelería, con el fin de personalizar su furgoneta de reparto"
+        description: "Creado para una papelería, con el fin de personalizar su furgoneta de reparto.",
+        tags: ["Cartel", "Premium"]
     },
     {
         id: "4",
         name: "Honda Civic",
         image: "/imgs/civic.jpeg",
-        description: "Diseñado y cortado para personalizar este Civic"
+        description: "Diseñado y cortado para personalizar este Civic.",
+        tags: ["Coche", "Estándar"]
     },
 ],[
     {
         id: "5",
         name: "Estanco",
         image: "/imgs/estanco.jpeg",
-        description: "Diseñado y cortado para arreglar el antiguo cartel que tenían puesto en la fachada."
+        description: "Diseñado y cortado para arreglar el antiguo cartel que tenían puesto en la fachada.",
+        tags: ["Cartel", "Premium"]
     },
     {
         id: "6",
         name: "Metro",
         image: "/imgs/metro.jpg",
-        description: "Creado para una furgoneta de la empresa."
+        description: "Creado para una furgoneta de la empresa.",
+        tags: ["Logo", "Estándar"]
     },
     {
         id: "7",
         name: "La Plazuela",
         image: "/imgs/plazuela.jpeg",
-        description: "Creado para una papelería, con el fin de personalizar su furgoneta de reparto"
+        description: "Creado para una papelería, con el fin de personalizar su furgoneta de reparto.",
+        tags: ["Cartel", "Premium"]
     },
     {
         id: "8",
         name: "Honda Civic",
         image: "/imgs/civic.jpeg",
-        description: "Diseñado y cortado para personalizar este Civic"
+        description: "Diseñado y cortado para personalizar este Civic.",
+        tags: ["Coche", "Estándar"]
     },
-
 ]]
