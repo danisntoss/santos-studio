be0dda9c3522d088ac3f938ab455f5035e1c382a
danisntoss <danisant1504.dsl@gmail.com>
2024-12-05 17:54:55 +0100

    added some media

--- src/data/data.js
+++ src/data/data.js
@@ -3,9 +3,9 @@ export const data = [[
         id: "1",
         name: "Estanco",
         images: [
-            "/imgs/estanco.jpeg",
-            "/imgs/estanco.jpeg",
-            "/imgs/estanco.jpeg",
+            "/imgs/estanco/1.jpeg",
+            "/imgs/estanco/2.jpeg",
+            "/imgs/estanco/3.jpeg"
         ],
         description: "Diseñado y cortado para arreglar el antiguo cartel que tenían puesto en la fachada.",
         tags: ["Cartel", "Premium"]
@@ -14,9 +14,7 @@ export const data = [[
         id: "2",
         name: "Metro",
         images: [
-            "/imgs/metro.jpg",
-            "/imgs/metro.jpg",
-            "/imgs/metro.jpg"
+            "/imgs/metro/1.jpg",
         ],
         description: "Creado para una furgoneta de la empresa.",
         tags: ["Logo", "Estándar"]
@@ -25,9 +23,10 @@ export const data = [[
         id: "3",
         name: "La Plazuela",
         images: [
-            "/imgs/plazuela.jpeg",
-            "/imgs/plazuela.jpeg",
-            "/imgs/plazuela.jpeg"
+            "/imgs/plazuela/1.jpeg",
+            "/imgs/plazuela/2.jpeg",
+            "/imgs/plazuela/3.jpeg",
+            "/imgs/plazuela/4.jpeg",
         ],
         description: "Creado para una papelería, con el fin de personalizar su furgoneta de reparto.",
         tags: ["Cartel", "Premium"]
@@ -36,11 +35,59 @@ export const data = [[
         id: "4",
         name: "Honda Civic",
         images: [
-            "/imgs/civic.jpeg",
-            "/imgs/civic.jpeg",
-            "/imgs/civic.jpeg"
+            "/imgs/civic/1.jpeg",
+            "/imgs/civic/2.jpeg",
+            "/imgs/civic/3.jpeg",
+            "/imgs/civic/4.jpeg",
+            "/imgs/civic/5.jpeg",
+            "/imgs/civic/6.jpeg",
+            "/imgs/civic/7.jpeg",
         ],
         description: "Diseñado y cortado para personalizar este Civic.",
         tags: ["Coche", "Estándar"]
     },
-]]
+],[
+    {
+        id: "5",
+        name: "ExteriorPlus",
+        images: [
+            "/imgs/exteriorplus/1.jpeg",
+            "/imgs/exteriorplus/2.jpeg",
+        ],
+        description: "Diseñado y cortado para arreglar el antiguo cartel que tenían puesto en la fachada.",
+        tags: ["Cartel", "Estándar"]
+    },
+    {
+        id: "6",
+        name: "Kawasaki",
+        images: [
+            "/imgs/kawasaki/1.jpeg",
+            "/imgs/kawasaki/2.jpeg",
+            "/imgs/kawasaki/3.jpeg",
+            "/imgs/kawasaki/4.jpeg",
+        ],
+        description: "Creado para una furgoneta de la empresa.",
+        tags: ["Moto", "Estándar"]
+    },
+    {
+        id: "7",
+        name: "Bowling",
+        images: [
+            "/imgs/bowling/1.jpeg",
+        ],
+        description: "Creado para una papelería, con el fin de personalizar su furgoneta de reparto.",
+        tags: ["logo", "Estándar"]
+    },
+    {
+        id: "8",
+        name: "Glovo",
+        images: [
+            "/imgs/glovo/1.jpeg",
+            "/imgs/glovo/2.jpeg",
+
+        ],
+        description: "Diseñado y cortado para personalizar este Civic.",
+        tags: ["Cartel", "Premium"]
+    },
+]
+]
